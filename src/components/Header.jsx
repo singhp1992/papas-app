1810bc702ef2f980d18a7c720a1c7028a93b4230
Preeti <singhp1992@gmail.com>
2019-09-25 18:08:08 +0200

    adding header css page to cmps

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2 className="welcome">Welcome to Pammi's Corner</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
