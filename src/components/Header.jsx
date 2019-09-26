e0a3648b27cf6e2bb31ca5669778270d298d4a4c
Preeti <singhp1992@gmail.com>
2019-09-26 20:10:07 +0200

    removing h2 header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <h2 className="welcome">Welcome to Pammi's Corner</h2>
                 <ul className="header-right"> 
                     <li className="welcome">Welcome to Pammi's Corner</li>
                     <li>About Me</li>
