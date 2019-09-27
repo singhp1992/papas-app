a586e28b0a1c8c4ba42e97acdb1440c8a08929ae
Preeti <singhp1992@gmail.com>
2019-09-27 16:39:21 +0200

    importing button to file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-
+import Button from 'react-bootstrap/Button';
 import './comps.css';
 
 class Header extends Component {
