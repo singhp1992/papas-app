bd28f4918be9ea404cb778195d0e671673d28e0c
Preeti <singhp1992@gmail.com>
2019-09-27 16:27:20 +0200

    deleting welcome css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
@@ -24,7 +24,7 @@ class Header extends Component {
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-success">Search</Button>
                     </Form>
-                </Navbar.Collapse>
+                </Navbar.Collapse> */}
             </Navbar>
         );
     }
