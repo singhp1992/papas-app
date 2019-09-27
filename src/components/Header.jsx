1401218880643e897618110a877781f2a426c2b2
Preeti <singhp1992@gmail.com>
2019-09-27 16:37:47 +0200

    importing nav drop down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import './comps.css';
 
 class Header extends Component {
