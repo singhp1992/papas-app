f6b852094f216de16cd35e3a86c8d2ad77221ce5
Preeti <singhp1992@gmail.com>
2019-09-30 11:58:20 +0200

    adding about me section to drop down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ class Header extends Component {
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown title="About Me" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
