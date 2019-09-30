e752ffc3c8e8c24ecd03e0c853464123ab02e1ff
Preeti <singhp1992@gmail.com>
2019-09-30 11:59:13 +0200

    adding medical history to drop down option

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ class Header extends Component {
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="About Me" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">How I started</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Medical Background</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
