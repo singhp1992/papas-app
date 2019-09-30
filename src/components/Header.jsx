b36264ca1ec223c4eeb57317ec37a07ca4cca5c8
Preeti <singhp1992@gmail.com>
2019-09-30 11:58:51 +0200

    changed dropdown 1 option

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ class Header extends Component {
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="About Me" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">How I started</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
