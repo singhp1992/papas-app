cb754dd6f2ceeaab709d814707f5e7a94c5b65fe
Preeti <singhp1992@gmail.com>
2019-10-03 11:16:17 +0200

    deleting unused bootstrap importgs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 import './comps.css';
 
 class Footer extends Component {
