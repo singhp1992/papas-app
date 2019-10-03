4303c1e0aad5547e703831452a6071e7b653caba
Preeti <singhp1992@gmail.com>
2019-10-03 11:14:23 +0200

    cleaning up code with semi colons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
