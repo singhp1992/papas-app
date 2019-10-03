ad80c658fc6a7196985166cf903325c450992216
Preeti <singhp1992@gmail.com>
2019-10-03 11:18:09 +0200

    changing drop down to nav links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,6 +14,7 @@ class Footer extends Component {
                         <Nav className="mr-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#shop">Shop</Nav.Link>
+                            <Nav.Link href="#about">About Me</Nav.Link>
                             <NavDropdown title="About Me" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Medical Background</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">How I started</NavDropdown.Item>
