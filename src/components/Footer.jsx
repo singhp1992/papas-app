653f1cf3f7aa246f8cce6ca6ab09ebb519735647
Preeti <singhp1992@gmail.com>
2019-10-03 11:39:41 +0200

    adding href shop to second nav link item

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ class Footer extends Component {
                         <Nav.Link href="/home">Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-1">Link</Nav.Link>
+                        <Nav.Link href="/shop" eventKey="link-1">Shop</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link eventKey="link-2">Link</Nav.Link>
