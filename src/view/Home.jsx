b45de30f46c07905b1baad5e32e820ec61de35b4
Preeti <singhp1992@gmail.com>
2019-10-03 12:50:06 +0200

    adding react layout to home page

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,3 +1,15 @@
 // add random quote generator api 
 // random moneky pic generator api 
 import React, { Component } from 'react';
+
+
+
+class Header extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Header;
